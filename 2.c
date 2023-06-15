2f1e3946f2e6f041827effa2e98d259f6e0e58db
jaiswalshweta021 <120053764+jaiswalshweta021@users.noreply.github.com>
2023-06-15 16:21:46 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,13 @@
+//write a program to print your biodata like name city state registration number
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("shweta jaiswal");
+	printf("\n12212160");
+	printf("\nBihar");
+	printf("\nChapra");
+	return 0;
+	
+ } 
+
